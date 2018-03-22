18c7327cbe58a83a5c10310c9c50f8d6bfe67b66
Stephen Burrows <stephen.r.burrows@gmail.com>
2018-03-21 22:40:12 -0700

    Added hamburger and search icons to home screen header

--- js/scenes/Home.js
+++ js/scenes/Home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Button, Text, View } from 'react-native'
 import type { NavigationScreenProp, NavigationStateRoute } from 'react-navigation'
 
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
 import styles from '../styles'
 
 type Props = {
@@ -9,10 +11,28 @@ type Props = {
 }
 
 export default class Home extends Component<Props> {
-	static navigationOptions = {
-		title: 'AO3 Unofficial',
+	static navigationOptions = ({ navigation }: Props) => ({
+		title: 'AO3 Unofficial App',
+		headerLeft: (
+			<Icon.Button
+				backgroundColor="#ffffff"
+				color="#000000"
+				name="menu"
+				onPress={() => navigation.navigate('DrawerOpen')}
+				size={30}
+			/>
+		),
+		headerRight: (
+			<Icon.Button
+				backgroundColor="#ffffff"
+				color="#000000"
+				name="search"
+				onPress={() => navigation.navigate('SearchStack')}
+				size={30}
+			/>
+		),
 		drawerLabel: 'Home',
-	}
+	})
 
 	render() {
 		return (
